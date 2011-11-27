30a628bed3d8eccd27e91f143c5c0c4b177ec24e
cheics <cheics@gmail.com>
2011-11-26 23:09:08 -0500

    New features guys... they work better now

--- MatlabPrototypes/FeatureDetection/featureDetect.m
+++ MatlabPrototypes/FeatureDetection/featureDetect.m
@@ -1,10 +1,15 @@
 basePath='C:\Users\cheics\Documents\Visual Studio 2010\Projects\';
 basePath2=strcat(basePath,'Beta2 Samples\Kinect-Tracking-Project\DataFiles\Experiment1\');
 
-
 reps_in_data=5;
-windowData=3;
-toKeep=0.8;
+windowData=0;
+toKeep=1;
+
+multiplyData=0;
+if multiplyData~=0
+    windowData=5;
+    toKeep=0.5;
+end
 
 headOn_dataIn_G1=importdata(strcat(basePath2,'headOnGood.csv'));
 headOn_dataIn_B1=importdata(strcat(basePath2,'headOnBadBack.csv'));
@@ -18,43 +23,43 @@ pp_dataIn_G1=importdata(strcat(basePath2,'profileGood.csv'));
 pp_dataIn_B1=importdata(strcat(basePath2,'profileBadBack.csv'));
 pp_dataIn_B2=importdata(strcat(basePath2,'profileBadHip.csv'));
 
-headOn_ts_G1=poseFinder(headOn_dataIn_G1.data(:,53), ...
-    5, windowData, toKeep);
-headOn_ts_B1=poseFinder(headOn_dataIn_B1.data(:,53), ...
-    5, windowData, toKeep);
-headOn_ts_B2=poseFinder(headOn_dataIn_B2.data(:,53), ...
-    5, windowData, toKeep);
+headOn_ts_G1=poseFinder(headOn_dataIn_G1.data, ...
+    reps_in_data, windowData, toKeep);
+headOn_ts_B1=poseFinder(headOn_dataIn_B1.data, ...
+    reps_in_data, windowData, toKeep);
+headOn_ts_B2=poseFinder(headOn_dataIn_B2.data, ...
+    reps_in_data, windowData, toKeep);
 
-ff_ts_G1=poseFinder(ff_dataIn_G1.data(:,53), ...
-    5, windowData, toKeep);
-ff_ts_B1=poseFinder(ff_dataIn_B1.data(:,53), ...
-    5, windowData, toKeep);
-ff_ts_B2=poseFinder(ff_dataIn_B2.data(:,53), ...
-    5, windowData, toKeep);
+% ff_ts_G1=poseFinder(ff_dataIn_G1.data(:,53), ...
+%     5, windowData, toKeep);
+% ff_ts_B1=poseFinder(ff_dataIn_B1.data(:,53), ...
+%     5, windowData, toKeep);
+% ff_ts_B2=poseFinder(ff_dataIn_B2.data(:,53), ...
+%     5, windowData, toKeep);
+% 
+% pp_ts_G1=poseFinder(pp_dataIn_G1.data(:,53), ...
+%     5, windowData, toKeep);
+% pp_ts_B1=poseFinder(pp_dataIn_B1.data(:,53), ...
+%     5, windowData, toKeep);
+% pp_ts_B2=poseFinder(pp_dataIn_B2.data(:,53), ...
+%     5, windowData, toKeep);
 
-pp_ts_G1=poseFinder(pp_dataIn_G1.data(:,53), ...
-    5, windowData, toKeep);
-pp_ts_B1=poseFinder(pp_dataIn_B1.data(:,53), ...
-    5, windowData, toKeep);
-pp_ts_B2=poseFinder(pp_dataIn_B2.data(:,53), ...
-    5, windowData, toKeep);
 
-
-
-% cc=GetFeatures(headOn_dataIn_G1.data, headOn_ts_G1);
-% dd=GetFeatures(headOn_dataIn_G1.data, headOn_ts_B1);
-% ee=GetFeatures(headOn_dataIn_B2.data, headOn_ts_B2);
+cc=GetFeatures(headOn_dataIn_G1.data, headOn_ts_G1);
+dd=GetFeatures(headOn_dataIn_B1.data, headOn_ts_B1);
+ee=GetFeatures(headOn_dataIn_B2.data, headOn_ts_B2);
 
 % cc=GetFeatures(ff_dataIn_G1.data, ff_ts_G1);
 % dd=GetFeatures(ff_dataIn_G1.data, ff_ts_B1);
 % ee=GetFeatures(ff_dataIn_B2.data, ff_ts_B2);
 
-cc=GetFeatures(pp_dataIn_G1.data, pp_ts_G1);
-dd=GetFeatures(pp_dataIn_G1.data, pp_ts_B1);
-ee=GetFeatures(pp_dataIn_B2.data, pp_ts_B2);
+% cc=GetFeatures(pp_dataIn_G1.data, pp_ts_G1);
+% dd=GetFeatures(pp_dataIn_G1.data, pp_ts_B1);
+% ee=GetFeatures(pp_dataIn_B2.data, pp_ts_B2);
 
 
 makePlots(cc, dd, ee);
+
 
 % plot3(cc(1,:), cc(2,:), cc(3,:), 'go', ...
 %     dd(1,:), dd(2,:), dd(3,:), 'ro', ...
--- MatlabPrototypes/FeatureDetection/poseFinder.m
+++ MatlabPrototypes/FeatureDetection/poseFinder.m
@@ -1,40 +1,51 @@
-function [ peakLocations ] = poseFinder(x, reps, dpw, np)
+function [ peakLocations ] = poseFinder(dataIn, reps, dpw, np)
 %poseFinder Summary of this function goes here
 %   x:      The input data
-%   rps:    Maximum number of reps to look for
-%   dps:    Desired around peak to sample poses
+%   reps:    Maximum number of reps to look for
+%   dpw:    Desired around peak to sample poses
 %   np:     Number on [0,1] to determine the percentage of 
 %               extra window value to keep for processing
     
+    yRightKneeData_ID=53;
+    x=dataIn(:, yRightKneeData_ID);
     fc = 0.5; % Cut-off frequency (Hz)
     fs = 20; % Sampling rate (Hz)
     order = 3; % Filter order
     [B,A] = butter(order,2*fc/fs); % [0:pi] maps to [0:1] here
     y=filtfilt(B,A,x); % LPF
-           
+
     [pks, loc]=findpeaks(y,...
-        'MINPEAKHEIGHT', (min(y)+max(y))/2, ...
+        'MINPEAKHEIGHT', (min(y)+max(y))/4, ...
         'MINPEAKDISTANCE', round(size(y, 1)./(reps*1.5)), ...
         'NPEAKS', reps+1);
+ 
+% % Peak debuging code
+%     plot(1:size(x, 1), y, 1:size(x, 1), x, loc, pks, 'ro');
+%     numPeaks=size(loc, 1);
+%     for n = 1:numPeaks
+%         debugPose(reshape(dataIn(loc(n), :), 3, 20)');
+%         pause
     
-    %plot(1:size(x, 1), y, 1:size(x, 1), x, loc, pks, 'ro');
-    
-    %Making shaky sampling window
-    samples=round(dpw*np);
-    numPeaks=size(loc, 1);
-    dstMat=zeros(numPeaks, 1+samples*2);
-    for n = 1:numPeaks
-        dd_neg=-dpw:-1;
-        dd_pos=1:dpw;
-        ss=sort(horzcat(randsample(dd_neg,samples),...
-                0, ...
-                randsample(dd_pos,samples)));
-        dstMat(n, :)=ss;
+    if dpw ~= 0
+        %Making shaky sampling window
+        samples=round(dpw*np);
+        numPeaks=size(loc, 1);
+        dstMat=zeros(numPeaks, 1+samples*2);
+        for n = 1:numPeaks
+            dd_neg=-dpw:-1;
+            dd_pos=1:dpw;
+            ss=sort(horzcat(randsample(dd_neg,samples),...
+                    0, ...
+                    randsample(dd_pos,samples)));
+            dstMat(n, :)=ss;
+        end
+
+        peakColumns=loc*ones(1, 1+samples*2)+ dstMat;
+        peakLocations=sort(reshape(peakColumns, ...
+            1, size(peakColumns, 1)*size(peakColumns, 2)));
+    else
+        peakLocations=loc;
     end
-    
-    peakColumns=loc*ones(1, 1+samples*2)+ dstMat;
-    peakLocations=reshape(peakColumns, ...
-        1, size(peakColumns, 1)*size(peakColumns, 2));
     
     
 end
