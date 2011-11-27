680576f28f47ee38df5bb2f69e444c79fa7d7ad0
cheics <cheics@gmail.com>
2011-11-27 12:11:21 -0500

    WHooops... changed the functions to use new feature set

--- MatlabPrototypes/FeatureDetection/GetFeatures.m
+++ MatlabPrototypes/FeatureDetection/GetFeatures.m
@@ -1,23 +1,25 @@
 function [ scoresArray ] = GetFeatures( bigData, valid_ts)
 
     kneeRight_IDS=[52,53,54];
-    hipRight_IDS=[43,44,45];
+    hipRight_IDS=[49,50,51];
     hipCentre_IDS=[1,2,3];
+    
+    shoulderCentre_IDS=[7,8,9];
+    shoulderLeft_IDS=[13,14,15];
+    shoulderRight_IDS=[25,26,27];
+    
     spine_IDS=[4,5,6];
     foot_IDS=[58,59,60];
 
-    ss=zeros(3,size(valid_ts,2));
-
-    for i = 1:size(valid_ts,2)
-        cc1=bigData(valid_ts(1,i), horzcat(kneeRight_IDS,hipCentre_IDS,spine_IDS));
-        cc2=bigData(valid_ts(1,i), horzcat(hipCentre_IDS,hipRight_IDS,kneeRight_IDS));
-        cc3=bigData(valid_ts(1,i), horzcat(hipRight_IDS,kneeRight_IDS, foot_IDS));
-
-        ss(1,i)=spineStability(cc1(:,1:3), cc1(:,4:6), cc1(:,7:9));
-        ss(2,i)=hipAngle(cc2(:,1:3), cc2(:,4:6), cc2(:,7:9));
-        ss(3,i)=kneeAngle(cc3(:,1:3), cc3(:,4:6), cc3(:,7:9));
+    cc=reshape(bigData, size(bigData,1), 3, 20);
+    poses=cc(valid_ts, :, :);
+        
+    scoresArray=zeros(3,size(poses,1));
+    for i = 1:size(poses,1)
+        specificPose=reshape(poses(i, :, :), 3, 20).';
+        scoresArray(1,i)=spineStability(specificPose);
+        scoresArray(2,i)=hipAngle(specificPose);
+        scoresArray(3,i)=kneeAngle(specificPose);
     end
-
-    scoresArray=ss;
 end
 
--- MatlabPrototypes/FeatureDetection/featureDetect.m
+++ MatlabPrototypes/FeatureDetection/featureDetect.m
@@ -10,7 +10,7 @@ reps_in_data=5;
 windowData=0;
 toKeep=1;
 
-multiplyData=0;
+multiplyData=1;
 if multiplyData~=0
     windowData=5;
     toKeep=0.5;
@@ -35,21 +35,40 @@ headOn_ts_B1=poseFinder(headOn_dataIn_B1.data, ...
 headOn_ts_B2=poseFinder(headOn_dataIn_B2.data, ...
     reps_in_data, windowData, toKeep);
 
-cc=GetFeatures(headOn_dataIn_G1.data, headOn_ts_G1);
-dd=GetFeatures(headOn_dataIn_B1.data, headOn_ts_B1);
-ee=GetFeatures(headOn_dataIn_B2.data, headOn_ts_B2);
+ff_ts_G1=poseFinder(ff_dataIn_G1.data, ...
+    reps_in_data, windowData, toKeep);
+ff_ts_B1=poseFinder(ff_dataIn_B1.data, ...
+    reps_in_data, windowData, toKeep);
+ff_ts_B2=poseFinder(ff_dataIn_B2.data, ...
+    reps_in_data, windowData, toKeep);
 
-% cc=GetFeatures(ff_dataIn_G1.data, ff_ts_G1);
-% dd=GetFeatures(ff_dataIn_G1.data, ff_ts_B1);
-% ee=GetFeatures(ff_dataIn_B2.data, ff_ts_B2);
+pp_ts_G1=poseFinder(pp_dataIn_G1.data, ...
+    reps_in_data, windowData, toKeep);
+pp_ts_B1=poseFinder(pp_dataIn_B1.data, ...
+    reps_in_data, windowData, toKeep);
+pp_ts_B2=poseFinder(pp_dataIn_B2.data, ...
+    reps_in_data, windowData, toKeep);
 
-% cc=GetFeatures(pp_dataIn_G1.data, pp_ts_G1);
-% dd=GetFeatures(pp_dataIn_G1.data, pp_ts_B1);
-% ee=GetFeatures(pp_dataIn_B2.data, pp_ts_B2);
+
+cc1=GetFeatures(headOn_dataIn_G1.data, headOn_ts_G1);
+dd1=GetFeatures(headOn_dataIn_B1.data, headOn_ts_B1);
+ee1=GetFeatures(headOn_dataIn_B2.data, headOn_ts_B2);
+
+cc2=GetFeatures(ff_dataIn_G1.data, ff_ts_G1);
+dd2=GetFeatures(ff_dataIn_G1.data, ff_ts_B1);
+ee2=GetFeatures(ff_dataIn_B2.data, ff_ts_B2);
+
+cc3=GetFeatures(pp_dataIn_G1.data, pp_ts_G1);
+dd3=GetFeatures(pp_dataIn_G1.data, pp_ts_B1);
+ee3=GetFeatures(pp_dataIn_B2.data, pp_ts_B2);
 
 
-makePlots(cc, dd, ee);
-score = clustering2(cc, dd, ee);
+%%%
+%%% 1 - frontal
+%%% 2 - 3/4 view
+%%% 3 - profile
+%makePlots(cc1, dd1, ee1);
+%score = clustering2(cc, dd, ee);
 
 % plot3(cc(1,:), cc(2,:), cc(3,:), 'go', ...
 %     dd(1,:), dd(2,:), dd(3,:), 'ro', ...
