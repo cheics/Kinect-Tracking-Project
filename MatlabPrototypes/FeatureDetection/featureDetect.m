1830bbadbaac4ac906ed9e2385b680df6883293e
cheics <cheics@gmail.com>
2011-11-26 23:34:21 -0500

    Adding auto peak detection code and new features

--- MatlabPrototypes/FeatureDetection/featureDetect.m
+++ MatlabPrototypes/FeatureDetection/featureDetect.m
@@ -1,28 +1,53 @@
 clc;
 
-basePath='C:\Users\Ameen Jamil\Desktop\SYDE461\cheics-Kinect-Tracking-Project-fc2e9bc';
-basePath2=strcat(basePath,'\DataFiles\Experiment1\');
+% basePath='C:\Users\Ameen Jamil\Desktop\SYDE461\cheics-Kinect-Tracking-Project-fc2e9bc';
+% basePath2=strcat(basePath,'\DataFiles\Experiment1\');
+
+basePath='C:\Users\cheics\Documents\Visual Studio 2010\Projects\';
+basePath2=strcat(basePath,'Beta2 Samples\Kinect-Tracking-Project\DataFiles\Experiment1\');
+
+reps_in_data=5;
+windowData=0;
+toKeep=1;
+
+multiplyData=0;
+if multiplyData~=0
+    windowData=5;
+    toKeep=0.5;
+end
 
 headOn_dataIn_G1=importdata(strcat(basePath2,'headOnGood.csv'));
 headOn_dataIn_B1=importdata(strcat(basePath2,'headOnBadBack.csv'));
 headOn_dataIn_B2=importdata(strcat(basePath2,'headOnBadHip.csv'));
 
-headOn_ts_G1=horzcat(40:70,175:195,290:310,405:435);
-headOn_ts_B1=horzcat(30:70,145:165,253:265,354:380,465:480);
-headOn_ts_B2=horzcat(50:60,165:170,273:278,376:383,485:495);
+ff_dataIn_G1=importdata(strcat(basePath2,'45Good.csv'));
+ff_dataIn_B1=importdata(strcat(basePath2,'45BadBack.csv'));
+ff_dataIn_B2=importdata(strcat(basePath2,'45BadHip.csv'));
 
-%plot(headOn_dataIn_G1.data(:,53));
+pp_dataIn_G1=importdata(strcat(basePath2,'profileGood.csv'));
+pp_dataIn_B1=importdata(strcat(basePath2,'profileBadBack.csv'));
+pp_dataIn_B2=importdata(strcat(basePath2,'profileBadHip.csv'));
 
-% ff_dataIn_G1=importdata(strcat(basePath2,'45Good.csv'));
-% ff_dataIn_B1=importdata(strcat(basePath2,'45BadBack.csv'));
-% ff_dataIn_B2=importdata(strcat(basePath2,'45BadHip.csv'));
-% 
-% 
+headOn_ts_G1=poseFinder(headOn_dataIn_G1.data, ...
+    reps_in_data, windowData, toKeep);
+headOn_ts_B1=poseFinder(headOn_dataIn_B1.data, ...
+    reps_in_data, windowData, toKeep);
+headOn_ts_B2=poseFinder(headOn_dataIn_B2.data, ...
+    reps_in_data, windowData, toKeep);
+
 cc=GetFeatures(headOn_dataIn_G1.data, headOn_ts_G1);
-dd=GetFeatures(headOn_dataIn_G1.data, headOn_ts_B1);
+dd=GetFeatures(headOn_dataIn_B1.data, headOn_ts_B1);
 ee=GetFeatures(headOn_dataIn_B2.data, headOn_ts_B2);
-% 
-% 
+
+% cc=GetFeatures(ff_dataIn_G1.data, ff_ts_G1);
+% dd=GetFeatures(ff_dataIn_G1.data, ff_ts_B1);
+% ee=GetFeatures(ff_dataIn_B2.data, ff_ts_B2);
+
+% cc=GetFeatures(pp_dataIn_G1.data, pp_ts_G1);
+% dd=GetFeatures(pp_dataIn_G1.data, pp_ts_B1);
+% ee=GetFeatures(pp_dataIn_B2.data, pp_ts_B2);
+
+
 makePlots(cc, dd, ee);
 score = clustering2(cc, dd, ee);
 
