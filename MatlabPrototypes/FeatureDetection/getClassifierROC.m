10fd64df33a50eb6ab16679512d97e1b0feeb158
cheics <cheics@gmail.com>
2012-03-11 15:32:58 -0400

    Get ROC setting fo rall excer

--- MatlabPrototypes/FeatureDetection/getClassifierROC.m
+++ MatlabPrototypes/FeatureDetection/getClassifierROC.m
@@ -2,14 +2,17 @@
 
 cc=MakeClassifier('GED', false);
 
-[tf,tc]=GetTrainingData('squats', 1);
-gedROC_squats=ROC_Curve(cc, tf, tc, 20, 25);
 
-[tf,tc]=GetTrainingData('armRaise', 1);
-gedROC_armRaise=ROC_Curve(cc, tf, tc, 20, 25);
+divs=20;
+pulls=25;
+[tf,tc]=getTrainingData('squats', 1);
+gedROC_squats=ROC_Curve(cc, tf, tc, divs, pulls);
 
-[tf,tc]=GetTrainingData('legRaise', 1);
-gedROC_legRaise=ROC_Curve(cc, tf, tc, 20, 25);
+[tf,tc]=getTrainingData('armRaise', 1);
+gedROC_armRaise=ROC_Curve(cc, tf, tc, divs, pulls);
 
-[tf,tc]=GetTrainingData('legExt', 1);
-gedROC_legExt=ROC_Curve(cc, tf, tc, 20, 25);
+[tf,tc]=getTrainingData('legRaise', 1);
+gedROC_legRaise=ROC_Curve(cc, tf, tc, divs, pulls);
+
+[tf,tc]=getTrainingData('legExt', 1);
+gedROC_legExt=ROC_Curve(cc, tf, tc, divs, pulls);
