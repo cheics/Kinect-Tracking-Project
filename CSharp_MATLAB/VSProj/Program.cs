3d5c7a73577b0c505d49fb00411563bc26245529
cheics <cheics@gmail.com>
2012-03-12 15:57:40 -0400

    Making it a bit more simple

--- CSharp_MATLAB/VSProj/Program.cs
+++ CSharp_MATLAB/VSProj/Program.cs
@@ -24,61 +24,41 @@ using System.Runtime.InteropServices;
 namespace example_ML_integration {
 	class Program {
 		static void Main(string[] args) {
-			////////////////////
-			// Input Parameters
-			////////////////////
-
 			// Using feature array as input for simplicity....
 			// Will Later send over the 30x(t) matrix for processing
-			System.Array ar = new double[] { 68.8, 120, 110, 118, 150, 179, 180, 150, 178, 152, 163, 107, 149, 97.8, 90.1, 94.3, 90.9, 89.9, 53.4 };
-
-
-			// create an array ai for the imaginary part of "a"
-			System.Array ai = new double[] { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 };
-
-
-			/////////////////////
-			// Output Parameters
-			/////////////////////
+			Boolean writeStuff=false;
+			double[] ar = new double[] { 68.8, 120, 110, 118, 150, 179, 180, 150, 178, 152, 163, 107, 149, 97.8, 90.1, 94.3, 90.9, 89.9, 53.4 };
+			double[,] ar_2d = new double[,] { { 1, 2 }, { 1, 2 } };
 
 			// initialize variables for return value from ML
 			System.Array cr = new double[3];
-			System.Array ci = new double[3];
 
+			// call MATLAB function
+			UseEngine(ar_2d, ref cr);
 
-			////////////////////////
-			// Call MATLAB function
-			////////////////////////
-			// call appropriate function/method based on Mode
-			UseEngine(ar, ai, ref cr, ref ci);
-
-
-			/////////////////////
-			// Output to console
-			/////////////////////
-			DisplayArgs(true, ar); // true = input
-			DisplayArgs(false, cr); // false = not-input or output
-			DisplayEnd();
+			// Output stuff..
+			if (writeStuff==true) {
+				Console.Clear();
+				Console.WriteLine("Input was:");
+				Console.WriteLine(String.Join(",", ar.Select(p => p.ToString()).ToArray()));
+				Console.WriteLine("Output was:");
+				double [] cr_d = new double[3];
+				cr_d = (double [])cr;
+				Console.WriteLine(String.Join(",", cr_d.Select(p => p.ToString()).ToArray()));
+				DisplayEnd();
+			}
 
 
 		}
-		static private void UseEngine(Array ar, Array ai, ref Array cr, ref Array ci) {
-			/*
-			 * This function calls the math_by_numbers routine inside
-			 * MATLAB using the MATLAB Engine's com interface
-			 */
-
+		static private void UseEngine(double[,] ar, ref Array cr) {
 			// Instantiate MATLAB Engine Interface through com
+
+			Console.WriteLine("Matlab Startup...\r\n");
 			MLApp.MLAppClass matlab = new MLApp.MLAppClass();
 
-			// Using Engine Interface, put the matrix "a" into 
-			// the base workspace.
-			// "a" is a complex variable with a real part of ar,
-			// and an imaginary part of ai
-			matlab.PutFullMatrix("a", "base", ar, ai);
+			// Make imaginary matricies
+			matlab.PutFullMatrix("a", "base", ar, ar);
 
-			// Using Engine Interface, execute the ML command
-			// contained in quotes.
 			String MyDocs = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
 			String ProjectLocation = "Visual Studio 2010\\Projects\\Kinect-Tracking-Project\\MatlabPrototypes\\FeatureDetection";
 			String matFileCD_command = String.Format("cd '{0}';", System.IO.Path.Combine(MyDocs, ProjectLocation));
@@ -88,42 +68,17 @@ namespace example_ML_integration {
 			matlab.Execute(matFileCD_command);
 			// matlab.Execute("open testBayes.m");
 			// matlab.Execute("dbstop in math_on_numbers.m");
+			Console.WriteLine("Matlab Processing...\r\n");
 			matlab.Execute("c = transpose(testBayes(a));");
 			
 			//matlab.Execute("com.mathworks.mlservices.MLEditorServices.closeAll");
 			//matlab.Execute("dbquit all");
-
-			// Using Engine Interface, get the matrix "c" from
-			// the base workspace.
-			// "c" is a complex variable with a real part of cr,
-			// and an imaginary part of ci
-			Console.WriteLine("Staring to do shit");
+			
+			
 			try {
-				matlab.GetFullMatrix("c", "base", ref cr, ref ci);
+				matlab.GetFullMatrix("c", "base", ref cr, ref cr);
 			} catch (Exception) {
 				Console.WriteLine("someErr");
-			}
-		}
-
-
-		static private void DisplayArgs(Boolean In, [In]Array OneReal) {
-			// --cheics: I swear there's a better way to do this. So sorry...
-			String outVals = "";
-			for (int i = 0; i < OneReal.Length; i++) {
-				outVals += OneReal.GetValue(i).ToString();
-				if (i != OneReal.Length - 1) {
-					outVals += ",";
-				}
-
-			}
-
-			if (In) {
-				// this is the first output, so
-				// prepare the console
-				Console.Clear();
-				Console.WriteLine("Input was: [{0}]\r\n", outVals);
-			} else {
-				Console.WriteLine("Output class using squatz classifier (should be [1,2,0]): [{0}]\r\n", outVals);
 			}
 		}
 
@@ -135,8 +90,8 @@ namespace example_ML_integration {
 
 			keypressed = Console.ReadKey(true);
 
-		} // end function DisplayEnd
+		}
 
-	} // end class Program
+	}
 
-} // end namespace example_ML_integration
+}
