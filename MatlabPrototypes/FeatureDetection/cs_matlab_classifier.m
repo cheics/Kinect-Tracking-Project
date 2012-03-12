8cf8d129fa3dbb847a34d4e81ea101c333770d01
cheics <cheics@gmail.com>
2012-03-12 19:04:18 -0400

    The thing that C-sharp can call

---
+++ MatlabPrototypes/FeatureDetection/cs_matlab_classifier.m
@@ -0,0 +1,36 @@
+function [outClass] = cs_matlab_classifier(exerciseType, kd, gp)
+	
+	existingTypes={'squats', 'legExt', 'legRaise', 'armRaise'};
+	if strcmp(exerciseType, 'squats')
+		[tf,tc]=getTrainingData('squats', 1);
+	elseif strcmp(exerciseType, 'armRaise')
+		[tf,tc]=getTrainingData('armRaise', 1);
+	elseif strcmp(exerciseType, 'legRaise')
+		[tf,tc]=getTrainingData('legRaise', 1);
+	elseif strcmp(exerciseType, 'legExt')
+		[tf,tc]=getTrainingData('legExt', 1);
+	else
+		err = MException('ResultChk:BadInput', ...
+			'Excercise type %s is not valid, must be one of {%s}',...
+			exerciseType, ...
+			strtrim(sprintf('%s ', existingTypes{:})) ...
+		);
+		throw(err)
+	end
+	
+	cc=MakeClassifier('BAYES', false);
+	cc.trainClassifiers(tf, tc);
+	
+	kk=FactoryKinectData_CS(kd, gp);
+	kk_features=kk.GetFeatures();
+	outClass=zeros(10,3)-1;
+	
+	for i=1:size(kk_features,1)
+		outClass(i,:)=cc.classify(kk_features(i,:));
+	end
+	
+end
+	
+	
+
+	
