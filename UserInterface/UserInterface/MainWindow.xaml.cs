be7e4eb8f373e46788077160f1ab16551c2b96bb
mehrad <mehradsl@gmail.com>
2012-03-12 21:41:52 -0400

    new changes

--- UserInterface/UserInterface/MainWindow.xaml.cs
+++ UserInterface/UserInterface/MainWindow.xaml.cs
@@ -19,6 +19,17 @@ using Microsoft.Windows.Controls;
 using MongoDB.Bson;
 using MongoDB.Driver;
 using Visiblox.Charts;
+using System.Collections.Generic;
+using MongoDB.Bson.IO;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Bson.Serialization.IdGenerators;
+using MongoDB.Bson.Serialization.Options;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver.Builders;
+using MongoDB.Driver.GridFS;
+using MongoDB.Driver.Wrappers;
 
 
 namespace UserInterface
@@ -37,11 +48,6 @@ namespace UserInterface
             comboBox2.Items.Add("Arm Raise");
             comboBox2.Items.Add("Lateral Leg Extension");
                                     
-        }
-
-        private void comboBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
         }
 
         private void comboBox2_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -77,37 +83,57 @@ namespace UserInterface
             else
                 comboBox1.Items.Add("");
         }
-        public class Party
+        public DateTime RandomDay()
         {
-            public string Name { set; get; }
-            public int Votes { set; get; }
-        }
+            DateTime start = new DateTime(2011, 1, 1);
+            Random gen = new Random();
 
+            int range = ((TimeSpan)(DateTime.Today - start)).Days;
+            return start.AddDays(gen.Next(range));
+        }
         private void button1_Click(object sender, RoutedEventArgs e)
         {
-            var query = new QueryDocument();
-           // query = 
-           // var query = Query.EQ("Name", "Mehrad");
-            MongoServer server = MongoServer.Create();
-            MongoDatabase exerHist = server.GetDatabase("exerHist");
-            MongoCollection<BsonDocument> exercises = exerHist.GetCollection<BsonDocument>("exercises");
-            MongoCollection<BsonDocument> hostory = exerHist.GetCollection<BsonDocument>("hostory");
-
-            DataSeries<double, double> cc1 = new DataSeries<double, double>();
-            DataSeries<double, double> cc2 = new DataSeries<double, double>();
-            DataSeries<double, double> cc3 = new DataSeries<double, double>();
-
-            foreach (BsonDocument instance in hostory.Find(query))
-            {
-                cc1.Add(new DataPoint<double,double>(){Y = instance["cc1"].ToInt32(),X= instance["time"].ToInt32() });
-                cc2.Add(new DataPoint<double, double>() { Y = instance["cc2"].ToInt32(), X = instance["time"].ToInt32() });
-                cc3.Add(new DataPoint<double, double>() { Y = instance["cc3"].ToInt32(), X = instance["time"].ToInt32() });
-            }
-
+            
             example.Series[0].DataSeries = null;
             example.Series[1].DataSeries = null;
             example.Series[2].DataSeries = null;
 
+            var query = new QueryDocument();
+
+            MongoServer server = MongoServer.Create();
+            MongoDatabase exerHist = server.GetDatabase("exerHist");
+            MongoCollection<BsonDocument> exercises = exerHist.GetCollection<BsonDocument>("exercises");
+            MongoCollection<BsonDocument> hostory = exerHist.GetCollection<BsonDocument>("hostory");
+            int b = 0;
+            while (b < 200) {
+                Random random = new Random();
+                int randomNumber = random.Next(0, 100);
+                int randomNumber1 = random.Next(0,2);
+                int randomNumber2 = random.Next(0,2);
+                int randomNumber3 = random.Next(0,2);
+                BsonDocument experiments = new BsonDocument { {"name", "Mehrad"}, {"exercise", "squats"}, {"cc1",randomNumber1 } , {"cc2", randomNumber2},
+                    {"cc3", randomNumber3}, {"time",RandomDay() }};
+                hostory.Insert(experiments);
+                b++;
+            }
+
+            DataSeries<DateTime, double> cc1 = new DataSeries<DateTime, double>(comboBox1.Items[0].ToString());
+            DataSeries<DateTime, double> cc2 = new DataSeries<DateTime, double>(comboBox1.Items[1].ToString());
+            DataSeries<DateTime, double> cc3 = new DataSeries<DateTime, double>(comboBox1.Items[2].ToString());
+            
+           // var sortBy = SortBy.Ascending("time");
+            
+           // hostory.Find().SetSortOrder(sortBy.Descending("time"));
+            IMongoSortBy sort = SortBy.Descending("time");
+
+            foreach (BsonDocument instance in hostory.Find(query).SetSortOrder(sort))
+            {
+                cc1.Add(new DataPoint<DateTime, double>() { Y = instance["cc1"].ToInt32(), X = instance["time"].AsDateTime });
+                cc2.Add(new DataPoint<DateTime, double>() { Y = instance["cc2"].ToInt32(), X = instance["time"].AsDateTime });
+                cc3.Add(new DataPoint<DateTime, double>() { Y = instance["cc3"].ToInt32(), X = instance["time"].AsDateTime });
+            }
+
+                      
             if (comboBox1.SelectedIndex == 0)
                 example.Series[0].DataSeries = cc1;
             else if (comboBox1.SelectedIndex == 1)      
@@ -120,14 +146,84 @@ namespace UserInterface
                 example.Series[1].DataSeries = cc2;
                 example.Series[2].DataSeries = cc3;
             }
+            cc1 = null;
+            cc2 = null;
+            cc3 = null;
 
         }
-
-        private void button2_Click(object sender, RoutedEventArgs e)
+        private void comboBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
         }
-    }
+        private void calendar2_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DailyChart.Series[0].DataSeries = null;
 
+            DataSeries<int, int> Dcc1 = new DataSeries<int, int>(comboBox1.Items[0].ToString());
+            DataSeries<int, int> Dcc2 = new DataSeries<int, int>(comboBox1.Items[1].ToString());
+            DataSeries<int, int> Dcc3 = new DataSeries<int, int>(comboBox1.Items[2].ToString());
+
+            MongoServer server = MongoServer.Create();
+            MongoDatabase exerHist = server.GetDatabase("exerHist");
+            MongoCollection<BsonDocument> exercises = exerHist.GetCollection<BsonDocument>("exercises");
+            MongoCollection<BsonDocument> hostory = exerHist.GetCollection<BsonDocument>("hostory");
+
+            var query = new QueryDocument();
+            int cc11 = 0, cc12 = 0, cc10 = 0, cc20 = 0, cc21 = 0, cc22 = 0, cc30 = 0, cc31 = 0, cc32 = 0;
+            var querytest1 = Query.EQ("time", calendar2.SelectedDate);
+
+            foreach (BsonDocument instance in hostory.Find(query))
+            {
+                string date = null;
+                string selecteddate = null;
+                DateTime daymonth;
+                DateTime selecteddaymonth;
+
+
+                daymonth = instance["time"].AsDateTime;
+                date = daymonth.Day.ToString() + "/" + daymonth.Month.ToString() + "/" + daymonth.Year.ToString();
+                selecteddaymonth = calendar2.SelectedDate.Value;
+                selecteddate = selecteddaymonth.Day.ToString() + "/" + selecteddaymonth.Month.ToString() + "/" + selecteddaymonth.Year.ToString();
+
+                if (date == selecteddate)
+                {
+                    if (instance["cc1"].ToInt32() == 0)
+                        cc10++;
+                    if (instance["cc1"].ToInt32() == 1)
+                        cc11++;
+                    if (instance["cc1"].ToInt32() == 2)
+                        cc12++;
+                    if (instance["cc2"].ToInt32() == 0)
+                        cc20++;
+                    if (instance["cc2"].ToInt32() == 1)
+                        cc21++;
+                    if (instance["cc2"].ToInt32() == 2)
+                        cc22++;
+                    if (instance["cc3"].ToInt32() == 0)
+                        cc30++;
+                    if (instance["cc2"].ToInt32() == 1)
+                        cc31++;
+                    if (instance["cc2"].ToInt32() == 2)
+                        cc32++;
+                }
+            }
+
+            Dcc1.Add(new DataPoint<int, int>() { Y = cc10, X = 0 });
+            Dcc1.Add(new DataPoint<int, int>() { Y = cc11, X = 1 });
+            Dcc1.Add(new DataPoint<int, int>() { Y = cc12, X = 2 });
+
+            Dcc2.Add(new DataPoint<int, int>() { Y = cc20, X = 0 });
+            Dcc2.Add(new DataPoint<int, int>() { Y = cc21, X = 1 });
+            Dcc2.Add(new DataPoint<int, int>() { Y = cc22, X = 2 });
+
+            Dcc3.Add(new DataPoint<int, int>() { Y = cc30, X = 0 });
+            Dcc3.Add(new DataPoint<int, int>() { Y = cc31, X = 1 });
+            Dcc3.Add(new DataPoint<int, int>() { Y = cc32, X = 2 });
+
+            DailyChart.Series[0].DataSeries = Dcc1;
+            DailyChart.Series[1].DataSeries = Dcc2;
+            DailyChart.Series[2].DataSeries = Dcc3;
+        }
+    }
 
 }
